25922db539af473b806f9db2d7d0ee4c10d0f765
Gaga Demetrashvili <gagademetrashvili@credo.ge>
2025-11-16 03:07:51 +0400

    Add default mapping rules and enable migrations
    Added default mapping rules to the `MappingRuleConfiguration` class using the `HasData` method for seeding the `MappingRules` table. Updated `appsettings.json` to enable automatic migrations on startup. Created a new migration class `SeedDefaultMappingRuleData` for inserting the default data, along with its designer file.

--- EventIngestionAPI/Infrastructure/Data/EntityFramework/EntityConfiguration/MappingRuleConfiguration.cs
+++ EventIngestionAPI/Infrastructure/Data/EntityFramework/EntityConfiguration/MappingRuleConfiguration.cs
@@ -1,4 +1,5 @@
 ﻿using EventIngestionAPI.Entities;
+using EventIngestionAPI.Enums;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -26,5 +27,98 @@ public class MappingRuleConfiguration : IEntityTypeConfiguration<MappingRule>
 
         builder.HasOne(mr => mr.MappingRuleType)
             .WithMany();
+
+        builder.HasData(
+            new MappingRule
+            {
+                Id = -1,
+                ExternalField = "playerId",
+                InternalField = "PlayerId",
+                MappingRuleTypeId = (int)MappingRuleTypeEnum.Default,
+                CreatedAt = new DateTime(2025, 11, 11),
+                IsActive = true
+            },
+            new MappingRule
+            {
+                Id = -2,
+                ExternalField = "userId",
+                InternalField = "PlayerId",
+                MappingRuleTypeId = (int)MappingRuleTypeEnum.Default,
+                CreatedAt = new DateTime(2025, 11, 11),
+                IsActive = true
+            },
+            new MappingRule
+            {
+                Id = -3,
+                ExternalField = "accountId",
+                InternalField = "PlayerId",
+                MappingRuleTypeId = (int)MappingRuleTypeEnum.Default,
+                CreatedAt = new DateTime(2025, 11, 11),
+                IsActive = true
+            },
+            new MappingRule
+            {
+                Id = -4,
+                ExternalField = "amount",
+                InternalField = "Amount",
+                MappingRuleTypeId = (int)MappingRuleTypeEnum.Default,
+                CreatedAt = new DateTime(2025, 11, 11),
+                IsActive = true
+            },
+            new MappingRule
+            {
+                Id = -5,
+                ExternalField = "value",
+                InternalField = "Amount",
+                MappingRuleTypeId = (int)MappingRuleTypeEnum.Default,
+                CreatedAt = new DateTime(2025, 11, 11),
+                IsActive = true
+            },
+            new MappingRule
+            {
+                Id = -6,
+                ExternalField = "currency",
+                InternalField = "Currency",
+                MappingRuleTypeId = (int)MappingRuleTypeEnum.Default,
+                CreatedAt = new DateTime(2025, 11, 11),
+                IsActive = true
+            },
+            new MappingRule
+            {
+                Id = -7,
+                ExternalField = "curr",
+                InternalField = "Currency",
+                MappingRuleTypeId = (int)MappingRuleTypeEnum.Default,
+                CreatedAt = new DateTime(2025, 11, 11),
+                IsActive = true
+            },
+            new MappingRule
+            {
+                Id = -8,
+                ExternalField = "timestamp",
+                InternalField = "OccurredAt",
+                MappingRuleTypeId = (int)MappingRuleTypeEnum.Default,
+                CreatedAt = new DateTime(2025, 11, 11),
+                IsActive = true
+            },
+            new MappingRule
+            {
+                Id = -9,
+                ExternalField = "occurredAt",
+                InternalField = "OccurredAt",
+                MappingRuleTypeId = (int)MappingRuleTypeEnum.Default,
+                CreatedAt = new DateTime(2025, 11, 11),
+                IsActive = true
+            },
+            new MappingRule
+            {
+                Id = -10,
+                ExternalField = "date",
+                InternalField = "OccurredAt",
+                MappingRuleTypeId = (int)MappingRuleTypeEnum.Default,
+                CreatedAt = new DateTime(2025, 11, 11),
+                IsActive = true
+            }
+       );
     }
 }
