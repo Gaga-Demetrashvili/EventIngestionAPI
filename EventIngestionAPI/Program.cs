e52a034b941ac699bcdcec79cc92e81d10498d44
Gaga Demetrashvili <gagademetrashvili@credo.ge>
2025-11-18 03:26:30 +0400

    Add Swagger support with Swashbuckle integration
    - Added `Swashbuckle.AspNetCore` package for Swagger documentation.
    - Configured Swagger generation with metadata in `Program.cs`.
    - Enabled Swagger middleware for UI and JSON endpoint.
    - Updated `launchSettings.json` to open Swagger UI on startup.

--- EventIngestionAPI/Program.cs
+++ EventIngestionAPI/Program.cs
@@ -3,6 +3,7 @@ using EventIngestionAPI.Infrastructure.Data.EntityFramework;
 using EventIngestionAPI.Infrastructure.RabbitMq;
 using EventIngestionAPI.Infrastructure.Services;
 using FluentValidation;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +12,29 @@ builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 builder.Services.AddSingleton<IEventMapper, EventMapper>();
 builder.Services.AddRabbitMqEventBus(builder.Configuration);
 
+builder.Services.AddSwaggerGen(s =>
+{
+    s.SwaggerDoc("v1", new OpenApiInfo()
+    {
+        Title = "Event Ingestion API",
+        Version = "v1",
+        Description = "Event Ingestion API for OnAim Tech Task",
+        Contact = new OpenApiContact
+        {
+            Name = "Gaga Demetrashvili",
+            Email = "gagademetrashvili6@gmail.com",
+            Url = new Uri("https://www.linkedin.com/in/gaga-demetrashvili-7247a0222/")
+        }
+    });
+});
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(s =>
+{
+    s.SwaggerEndpoint("/swagger/v1/swagger.json", "Event Ingestion API V1");
+});
 
 app.RegisterEventIngestionApiEndpoints();
 app.RegisterMappingRuleApiEndpoints();
